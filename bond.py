a4e44ac76e338c95709cf48b3de6da1b451431ff
oscarjohnson04 <oscarlloydjohnson@gmail.com>
2025-07-30 23:48:52 -0400

    bond.py

--- bond.py
+++ bond.py
@@ -40,51 +40,42 @@ with tab1:
         default_date -= dt.timedelta(days=default_date.weekday() - 4)
     selected_date = st.date_input("Select a date", value=default_date, min_value=dt.date(2002, 1, 1))
     date = pd.to_datetime(selected_date)
-
-    selected_bonds = st.multiselect(
-        "Select bonds to display:",
-        options=list(series_ids.keys()),
-        default=list(series_ids.keys())
-    )
-
-    if selected_bonds:
-        @st.cache_data(show_spinner=False)
-        def fetch_yield_on_date(date, selected_bonds):
-            data = {}
-            for label in selected_bonds:
-                code = series_ids[label]
-                try:
-                    value = fred.get_series(code).get(date, np.nan)
-                except:
-                    value = np.nan
+    
+    @st.cache_data(show_spinner=False)
+    def fetch_yield_curve(date):
+        data = {}
+        for label, code in series_ids.items():
+            try:
+                value = fred.get_series(code).get(date, np.nan)
+            except:
+                value = np.nan
                 data[label] = value
-            return pd.DataFrame([data])
+        return pd.DataFrame([data])
 
-        df = fetch_yield_on_date(date, selected_bonds)
-        df_t = df.T
-        df_t.columns = ['Rate']
-        df_t = df_t.reset_index().rename(columns={'index': 'Maturity'})
+    df = fetch_yield_curve(date)
+    
+    df_t = df.T
+    df_t.columns = ['Rate']
+    df_t = df_t.reset_index().rename(columns={'index': 'Maturity'})
 
-        st.dataframe(df_t.set_index("Maturity"), use_container_width=True)
+    st.dataframe(df_t.set_index("Maturity"), use_container_width=True)
 
-        fig = go.Figure()
-        fig.add_trace(go.Scatter(
-            x=df_t['Maturity'],
-            y=df_t['Rate'],
-            mode='lines+markers',
-            line=dict(color='royalblue', width=3),
-            marker=dict(size=8),
-            name=f'Yields on {date.strftime("%Y-%m-%d")}'
-        ))
-        fig.update_layout(
-            title=f'US Treasury Yield Curve on {date.strftime("%Y-%m-%d")}',
-            xaxis_title='Maturity',
-            yaxis_title='Yield (%)',
-            template='plotly_white'
-        )
-        st.plotly_chart(fig, use_container_width=True)
-    else:
-        st.info("Select at least one bond maturity to display the yield curve.")
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(
+        x=df_t['Maturity'],
+        y=df_t['Rate'],
+        mode='lines+markers',
+        line=dict(color='royalblue', width=3),
+        marker=dict(size=8),
+        name=f'Yields on {date.strftime("%Y-%m-%d")}'
+    ))
+    fig.update_layout(
+        title=f'US Treasury Yield Curve on {date.strftime("%Y-%m-%d")}',
+        xaxis_title='Maturity',
+        yaxis_title='Yield (%)',
+        template='plotly_white'
+    )
+    st.plotly_chart(fig, use_container_width=True)
 
 # --------------------------------------------------------
 # 📈 Tab 2: Historical Yield Plotter
